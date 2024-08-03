664d13c71f03b11e8d897a25ed3a405da9421d73
anjali <anjalichimata21@gmail.com>
2024-08-03 15:24:27 +0000

    Congrats-Card

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className='container'>
+    <div>
+      <h1>Congratulations</h1>
+    </div>
+    <div className='person-card'>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' />
+      <h1>Kiran V</h1>
+      <p>
+        Vishnu Institute of Computer Education and Technology.
+        <br />
+        Bhimavaram
+      </p>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
